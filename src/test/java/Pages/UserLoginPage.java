07bab6486d25f5ceb5f99cbbd4edb48fa66af548
R.O. <ramos16qa@gmail.com>
2025-07-21 22:09:22 -0700

    Added Login Test class

--- src/test/java/Pages/UserLoginPage.java
+++ src/test/java/Pages/UserLoginPage.java
@@ -1,4 +1,17 @@
 package Pages;
 
-public class UserLoginPage {
+import Base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class UserLoginPage extends BaseTest {
+
+    @Test
+    public void testUserLogin(){
+        driver.get("https://automationexercise.com/login");
+        driver.findElement(By.name("email")).sendKeys("tes0001tUser@gmail.com");
+        driver.findElement(By.xpath("//input[@data-qa='login-password']")).sendKeys("Password12345");
+        driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+
+}
 }
--- src/test/java/Tests/UserRegistrationTest.java
+++ src/test/java/Tests/UserRegistrationTest.java
@@ -3,18 +3,15 @@ import Base.BaseTest;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.time.Duration;
 
 public class UserRegistrationTest extends BaseTest {
 
     @Test
-    public void UserRegistration(){
+    public void testUserRegistration(){
 
         driver.get("https://automationexercise.com/login");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.name("name")).sendKeys("TestUser");
-        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("tes01tUser@gmail.com");
+        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("tes0001tUser@gmail.com");
         driver.findElement(By.xpath("//button[@data-qa='signup-button']")).click();
         driver.findElement(By.id("id_gender1")).click();
         driver.findElement(By.id("password")).sendKeys("Password12345");
